db6bf2693db277786604f03c8e7ad7ba2cb86395
Your Name <youremail@example.com>
2026-02-19 01:43:47 +0000

    test

--- index.js
+++ index.js
@@ -53,10 +53,8 @@ function saveJson(fileName, data) {
 
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
   console.log("✅ MongoDB Connected");
 }).catch(err => {
   console.error("❌ MongoDB Connection Error:", err);
